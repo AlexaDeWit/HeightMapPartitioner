a49645476166daf3ead064bc97906f143ef0dfba
Alexa DeWit <alexa.dewit@gmail.com>
2017-03-11 19:36:00 +0100

    Basic zero tests yolo version is now implemented, enjoy kids

--- src/partitioner.rb
+++ src/partitioner.rb
@@ -1,4 +1,5 @@
 require 'rmagick'
+require 'fileutils'
 
 class Partitioner
 
@@ -6,22 +7,22 @@ class Partitioner
 
   def initialize(filepath, partition_count)
     @filepath = filepath
-    @partition_count = partition_count
+    @partition_count = Integer(partition_count)
   end
 
   def is_valid_settings?
     @image = ImageList.new(@filepath).first
-    return false unless @image
+    return false unless @image && @partition_count
     if @image.columns != @image.rows
       puts "Image height and width in pixels must be identical"
       return false
     end
-    if (Math.log2(@image.columns) % 1) != 0
-      puts "Image dimensions must be identical powers of 2"
+    if @image.columns % 2 != 1
+      puts "Image dimensions must be odd numbers."
       return false
     end
-    if (@image.columns / @partition_count) != 0
-      puts "Image width must be exactly divisble by the partition count"
+    if ((@image.columns - 1) % @partition_count) != 0
+      puts "Image dimensions - 1 must be exactly divisble by the partition count"
       return false
     end
     if (Math.log2(@partition_count) % 1) != 0
@@ -32,6 +33,26 @@ class Partitioner
   end
 
   def run!
+    #for safety sake
+    return false unless is_valid_settings?
+    create_or_load_output_directory
+    #okay lets slice it.
+    step_size = @image.columns / @partition_count
+    chunk_size = step_size + 1
+    (0..(@partition_count - 1)).each do |y|
+      (0..(@partition_count - 1)).each do |x|
+        subsection = @image.crop(step_size * x + 1, step_size * y + 1, chunk_size, chunk_size)
+          subsection.format = 'png'
+          full_path = "#{@output_directory}/Heightmap_x#{x}_y#{y}.png"
+          subsection.write(full_path)
+      end
+    end
+    puts "Done, look in the heightmap_partition_output directory for your new babies!"
+  end
+
+  def create_or_load_output_directory
+    Dir.mkdir(File.join(Dir.pwd, "heightmap_partition_output"), 0700) unless Dir.exist?(File.join(Dir.pwd, "heightmap_partition_output"))
+    @output_directory = File.join(Dir.pwd, "heightmap_partition_output")
   end
 
 end
